2df0028197923014d800637c93161747ca1396b2
claem <magdaguirre@gmail.com>
2014-07-12 20:15:39 -0700

    package added required to make ccat work

--- modules/tools/manifests/init.pp
+++ modules/tools/manifests/init.pp
@@ -1,11 +1,12 @@
 class tools {
 
   package { 
-    'screen':     ensure => present;
-    'tree':       ensure => present;
-    'htop':       ensure => present;
-    'vim':        ensure => present;
-    'python-pip': ensure => present;
-  }
+    'screen':           ensure => present;
+    'tree':             ensure => present;
+    'htop':             ensure => present;
+    'vim':              ensure => present;
+    'python-pip':       ensure => present;
+    'python-pygments':  ensure => present;  
+ }
 
 }
