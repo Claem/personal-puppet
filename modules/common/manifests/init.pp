e4ec59da1c068db994c7e3a8419538e5853fafc4
claem <magdaguirre@gmail.com>
2014-09-03 15:34:32 -0700

    vim tmp directory added for universal swap file storage

--- modules/common/manifests/init.pp
+++ modules/common/manifests/init.pp
@@ -1,5 +1,12 @@
 class common {
 
-  include 'common::defines'
+  include 'common::defines'  
+
+file { '~/.vim-tmp':
+  ensure => directory,
+  owner  => root,
+  group  => root,
+  mode   => 644,
+  }
 
 }
